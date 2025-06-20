76300c0c989660a5229c1a19b311cf3c793fa9b5
Michael Duran <michaeldmedina05@gmail.com>
2025-06-20 15:43:11 +0000

    juego terminado

--- src/app.js
+++ src/app.js
@@ -1,3 +1,41 @@
 //write your code here
-console.log("Hello Rigo from the console!");
 
+     let classicgame = ["rock", "paper", "scissors", "lizard", "spock"];
+     
+     let selectOptions = {
+        rock : ["Scissors", "lizard"],
+        paper : ["rock", "spock"],
+        scissors : ["paper", "lizard"],
+        lizard : ["paper", "spock"],
+        spock : ["scissors", "rock"]
+     };
+    
+    
+     function Play(YourChoise) 
+     {
+         YourChoise.toLowerCase()
+         const ComputerChoise = classicgame[Math.floor(Math.random() * classicgame.length)]
+         console.log(`seleccion de la IA: ${ComputerChoise}`);
+         console.log(`seleccion del jugador: ${YourChoise}`);
+         
+         if(YourChoise === ComputerChoise)
+            {
+                console.log("empate");
+            }
+            else if(selectOptions[YourChoise].includes(ComputerChoise))
+                {
+                    console.log("tu ganas");    
+                }
+                else
+                {
+                    console.log("tu pierdes");
+                    
+                }  
+            
+        } 
+    Play("rock");
+    
+    
+    
+    
+console.log("Hello Rigo from the console!");
