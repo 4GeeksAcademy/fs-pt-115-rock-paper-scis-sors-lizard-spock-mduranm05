79c8a35da7f8a97548cd24eb39da269edcaf8bdd
Michael Duran <michaeldmedina05@gmail.com>
2025-06-25 18:15:35 +0000

    he mejorado la legibilidad

--- src/app.js
+++ src/app.js
@@ -1,41 +1,42 @@
 //write your code here
 
-     let classicgame = ["rock", "paper", "scissors", "lizard", "spock"];
-     
-     let selectOptions = {
-        rock : ["Scissors", "lizard"],
-        paper : ["rock", "spock"],
-        scissors : ["paper", "lizard"],
-        lizard : ["paper", "spock"],
-        spock : ["scissors", "rock"]
-     };
-    
-    
-     function Play(YourChoise) 
-     {
-         YourChoise.toLowerCase()
-         const ComputerChoise = classicgame[Math.floor(Math.random() * classicgame.length)]
-         console.log(`seleccion de la IA: ${ComputerChoise}`);
-         console.log(`seleccion del jugador: ${YourChoise}`);
-         
-         if(YourChoise === ComputerChoise)
-            {
-                console.log("empate");
-            }
-            else if(selectOptions[YourChoise].includes(ComputerChoise))
-                {
-                    console.log("tu ganas");    
-                }
-                else
-                {
-                    console.log("tu pierdes");
-                    
-                }  
-            
-        } 
-    Play("rock");
-    
-    
-    
-    
+let classicgame = ["rock", "paper", "scissors", "lizard", "spock"];
+
+let selectOptions = {
+    rock: ["Scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["paper", "spock"],
+    spock: ["scissors", "rock"]
+};
+
+
+function Play(YourChoise) {
+    YourChoise.toLowerCase()
+    const ComputerChoise = classicgame[Math.floor(Math.random() * classicgame.length)]
+    console.log(`seleccion de la IA: ${ComputerChoise}`);
+    console.log(`seleccion del jugador: ${YourChoise}`);
+
+    if (YourChoise === ComputerChoise)
+        console.log("empate");
+
+    if (selectOptions[YourChoise].includes(ComputerChoise))
+        console.log("tu ganas");
+    else console.log("tu pierdes");
+
+    //else if(selectOptions[YourChoise].includes(ComputerChoise))
+    //{
+    //  console.log("tu ganas");    
+    //}
+    //else
+    //{
+    //  console.log("tu pierdes");  
+    //}  
+
+}
+Play("paper");
+
+
+
+
 console.log("Hello Rigo from the console!");
